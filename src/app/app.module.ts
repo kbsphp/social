2f9c9b9c37ca4db32b0d9e1bce136229c2fedeae
kbsphp <sunny@gmail.com>
2019-07-01 16:12:52 +0530

    design implement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { GeneralFeedComponent } from './components/general-feed/general-feed.component';
+import { UserFeedComponent } from './components/user-feed/user-feed.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GeneralFeedComponent,
+    UserFeedComponent,
+   
   ],
   imports: [
     BrowserModule,
